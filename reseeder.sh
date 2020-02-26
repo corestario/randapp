54de17914889d8a09d8ad3797a0a4545557f77ed
andrey.rybnov <ryban92@gmail.com>
2020-02-26 15:17:33 +0300

    Add reseeder

---
+++ reseeder.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# begin
+
+reseed_interval=$1
+#use for reseeding module access
+sender=""
+
+echo "reseed every $reseed_interval blocks"
+
+function get_last_block_height() {
+  local hs=$(rcli q block --trust-node | jq '.block_meta.header.height | tonumber')
+  echo $hs
+}
+
+
+function get_btc_hash() {
+  local hsh=$(curl https://blockchain.info/latestblock | jq .hash)
+  echo $hsh
+  if [ -z $hsh ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+prevHeight=$(get_last_block_height)
+echo last block height = $prevHeight
+
+
+function is_time_to_reseed() {
+  local lb=$(get_last_block_height)
+  local pH=$prevHeight
+  local diff=$(($lb - $pH))
+  echo $lb
+  if [ $diff -ge $reseed_interval ]
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+
+while [ 1 ]
+do
+  r=$(is_time_to_reseed)
+  r2=$?
+  if [ $r2 -eq 0 ]
+  then
+    echo "time to reseed"
+    prevHeight=$r
+    seed=$(get_btc_hash)
+    s2=$?
+    if [ $s2 -eq 0 ]
+    then
+      # use reseeding module here
+      # rcli reseeding send $sender $seed
+    fi
+  fi
+  echo sleeeep
+  sleep 2
+done
+
+# end
